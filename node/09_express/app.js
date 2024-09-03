54639aa246339f4470f0a62f382553ae1684d7dd
1093661893 <1093661893@qq.com>
2024-09-03 03:26:55 +0000

    next: db

--- node/09_express/app.js
+++ node/09_express/app.js
@@ -3,6 +3,7 @@ import test from './routes/test.js';
 import sys from './routes/sys.js';
 import response from './routes/response.js';
 import header from './routes/header.js';
+import upload from './routes/upload.js';
 const app = express();
 const post = 80
 // .json中间件用来解析body的数据，这是因为 express 默认不支持解析传递的请求体（body）数据
@@ -14,10 +15,12 @@ app.use((req,res,next) => {
     console.log(query, body, params)
     next()
 })
+app.use(express.static('public'))
 test(app) // 普通模式
 app.use('/sys', sys) // Router模式 // 将路由注册到/sys下，所有路由都会带上/sys
 app.use('/res', response) // 响应数据
 app.use('/header', header) // 请求头
+app.use(upload) // 文件上传
 
 app.listen(post, () => {
     console.log(`server is running at http://localhost:${post}`)
---
+++ node/09_express/routes/upload.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import fs from 'fs'
+import multer from 'multer'
+const router = express.Router();
+const store = multer.diskStorage({
+    // 指定文件存储目录
+    destination: (req, file, cb) => {
+        const dir = '../newFold'
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir)
+            console.log(fs.existsSync(dir))
+        }
+        cb(null, dir)
+    },
+    // 指定文件命名规则
+    filename: (req, file, cb) => {
+        const ext = file.originalname.split('.').pop() // 获取文件后缀名
+        cb(null, `${Date.now()}.${ext}`)
+    }
+})
+const upload = multer({
+    storage: store,
+    limits: { fileSize: 1024 * 1024 * 5 }, //文件大小限制
+    fileFilter: (req, file, cb) => {
+        // fileFilter() 函数指定了文件类型过滤规则
+        // 拒绝上传非图片类型的文件
+        if (!file.mimetype.startsWith('image/')) {
+            const err = new Error('Only image files are allowed!') // 错误的具体信息
+            err.status = 400 // 设置错误状态码为 400
+            return cb(err, false)
+        }
+        return cb(null, true)
+    }
+})
+// single() 方法用于处理单文件上传
+router.post('/upload',upload.single('file'), (req, res) => {
+    res.json({ message: '文件上传成功', data: req.file })
+})
+export default router
